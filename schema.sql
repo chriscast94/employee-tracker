ffe532ad0372acca7bc0197eff9e1849944a6eb2
Christian Castillo <chriscast94@outlook.com>
2021-11-03 09:17:05 -0700

    initial commit; starting code

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Create Department table with id and name
+CREATE TABLE department (
+    department_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+--Create role/position table with id, title, salary, and department id
+CREATE TABLE position (
+    position_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(9,2),
+    department_id INT REFERENCES department(department_id)
+);
+
+--Create employee table with id, first name, last name, role id, and manager id
+CREATE TABLE employee (
+    employee_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL REFERENCES position(position_id),
+    manager_id INT REFERENCES employee(employee_id)
+);
---
+++ seeds.sql
@@ -0,0 +1,5 @@
+--Prepopulate the database
+INSERT INTO department (name)
+VALUE ('Marketing');
+INSERT INTO position (title, salary, department_id)
+VALUE ('Outreach', 55000, 1)
